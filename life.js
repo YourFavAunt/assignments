5321d4b267f8f333f68633fd3929f56713a81e4f
YourFavAunt <105464435+YourFavAunt@users.noreply.github.com>
2022-06-24 09:40:24 +0100

    first js task

---
+++ life.js
@@ -0,0 +1,15 @@
+//toestMusicianInNigeria = 'davido' decleare a variable we use the let keyword, avriables are containers for storing data//
+let country= 'Nigeria';
+let height = "5'6";
+let name = "FORTUNE";
+var age =  17;  //const here means constant and the value cannot be reassigned unlike var and let//
+
+console.log("my name is " + name )
+console.log("I am " + height)
+console.log("I am from " + country)
+   //you can also createa variable by using const or var//
+   /**you cannot start your variablr name with a number
+   your var name should not have space or hyphen
+   the fist word in your variable name should start with a small letter, the subsequent ones should have a capitalletter eg ln3
+    **/
+   
